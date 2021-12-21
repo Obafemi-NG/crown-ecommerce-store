dc1a0200fea9f773e3ca9b252bcdf47b32fc5f0b
obafemi <oludahunsiobafemi18@gmail.com>
2021-12-21 14:54:20 +0100

    created the "shop" page, with the background image for each collection item indicating the kind of product.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.components';
+import Shop from './pages/shop/shop.components'
 import { Route, Routes } from 'react-router-dom';
 
 
 
 import './App.css';
 
-const Hats = () => {
-  return (
-    <h1>Your Hats here!!!</h1>
-  )
-}
-
 class App extends React.Component {
   render() {
     return (
       <Routes>
         <Route path = '/' element = {<HomePage/>} />
-        <Route path = '/hats' element = {<Hats/>} />
+        <Route path = '/shop' element = {<Shop/>} />
       </Routes>
       
     );
