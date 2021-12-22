43d29c69b89a007ddd5268f3e7e1038a28569ac4
obafemi <oludahunsiobafemi18@gmail.com>
2021-12-22 10:48:31 +0100

    created a custom  styled button for sign in and other fuctions that needed later on in this project

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormInput from '../forminput/forminput.component';
+import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -30,7 +31,7 @@ class SignIn extends React.Component{
         return (
             <React.Fragment>
                 <div className = 'sign-in'>
-                    <h2>HAVE AN ACCOUNT WITH US ALREADY?</h2>
+                    <h2 className = 'title'>HAVE AN ACCOUNT WITH US ALREADY?</h2>
                     <span>Sign in with your Email and Password</span>
                     <form onSubmit = {this.handleSubmmit}>
                         <FormInput type = 'text' name = 'email' value = {this.state.email} onChange = {this.handleChange} label = 'email' />
@@ -39,7 +40,7 @@ class SignIn extends React.Component{
                         <FormInput type = 'password' name = 'password' value = {this.state.password} onChange = {this.handleChange} label = 'password'  />
                         
 
-                        <input type = 'submit' value = 'Submit Form' />
+                        <CustomButton type = 'submit' value = 'SIGN IN' > Sign in</CustomButton>
                     </form>
                 </div>
             </React.Fragment>
