cdd83c24915956e44b1485e1d65d6271e58b935d
obafemi <oludahunsiobafemi18@gmail.com>
2022-01-05 15:37:15 +0100

    updated the css to the BEM system of CSS in JS for the custom buttons and all asscociated buttons with different props

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './custom-button.styles.scss';
+import { CustomButtonContainer } from './custom-button.styles';
 
-const CustomButton = ({children, inverted, isGoogleSignIn, ...otherProps}) => {
+const CustomButton = ({children, ...props}) => {
     return(
-        <button className ={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'signInGoogle' : ''} custom-button`}  {...otherProps} >
+        <CustomButtonContainer {...props} >
             {children}
-        </button>
+        </CustomButtonContainer>
     )
 }
 
